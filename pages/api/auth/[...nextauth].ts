8e64cbe08bf0468fe03efa42059a638b98a03c16
jackmayr2 <jackmayr2@gmail.com>
2025-02-20 19:19:42 -0500

    content creation fix

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -18,7 +18,8 @@ export const authOptions: AuthOptions = {
                         "profile",
                         "https://www.googleapis.com/auth/drive.file",
                         "https://www.googleapis.com/auth/drive.readonly",
-                        "https://www.googleapis.com/auth/drive.metadata.readonly"
+                        "https://www.googleapis.com/auth/drive.metadata.readonly",
+                        "https://www.googleapis.com/auth/documents"
                     ].join(" ")
                 }
             }
