a5f6cec92a8b1cd031d617dd6f8a9e44e85c8a50
jackmayr2 <jackmayr2@gmail.com>
2025-02-27 00:28:17 -0500

    vercel fix2

--- src/components/index.ts
+++ src/components/index.ts
@@ -10,7 +10,6 @@ export { default as SignUpFlow } from './auth/SignUpFlow';
 // Feature components
 export { default as DrivePicker } from './DrivePicker';
 export { default as PricingTable } from './pricing/PricingTable';
-export { default as EmailConnectModal } from './EmailConnectModal';
 export { default as GmailLabelSelector } from './GmailLabelSelector';
 
 // Note: Only export components that are meant to be used across multiple pages 
